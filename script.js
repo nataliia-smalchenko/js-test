02822c6a7d5042a4aad5a337e109ccff401f6dc1
Nataliia Smalchenko <137941820+nataliia-smalchenko@users.noreply.github.com>
2024-07-09 18:32:12 +0300

    lesson 1

---
+++ script.js
@@ -0,0 +1,27 @@
+// console.log("Hello!");
+
+// let c = [2, 3, 12, 3];
+
+// let a = "sdsd";
+// console.log(a);
+// const b = 3;
+// console.log(b);
+
+// a = Math.sqrt(4);
+
+// while (a < 10) {
+//   console.log(a);
+//   a += 1;
+// }
+
+// for (let i = 0; i < 10; i += 1) {
+//   console.log(i);
+// }
+
+// if (0) {
+//   console.log("sd");
+// } else if (1) {
+//   console.log("s");
+// } else {
+//   console.log("d");
+// }
